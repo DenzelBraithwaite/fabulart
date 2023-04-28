ac644ab7d486e5d1e957aa6b7680f4403f6c42cc
Denzel Braithwaite <denzelbraithwaite@Denzels-MacBook-Pro.local>
2023-04-28 07:39:40 -0400

    created buttons

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -4,21 +4,22 @@
     export let shape = 'square'
     export let url = '/'
     export let border = false;
+    export let borderColor = 'border-peach';
     $: classes = `${textColor} ${bgColor} ${shape}`
 </script>
 
 <!-- TODO:
-- Make button shape settable from outside✅
-- Make text color settable from outside✅
-- Make background color settable from outside✅
-- Make border settable from outside
-- Make border color settable from outside
+- Make button shape settable from outside ✅
+- Make text color settable from outside ✅
+- Make background color settable from outside ✅
+- Make border settable from outside ✅
+- Make border color settable from outside ✅
 
 
 
 -->
 {#if border}
-<a href="{url}" class="btn border {classes}"><slot>Try me</slot></a>
+<a href="{url}" class="btn border {classes} {borderColor}"><slot>Try me</slot></a>
 {:else}
 <a href="{url}" class="btn no-border {classes}"><slot>Try me</slot></a>
 {/if}
@@ -28,7 +29,6 @@
     .btn:visited {
         display: inline-block;
         text-decoration: none;
-        color: #fff;
         text-align: center;
         padding: 0.5rem 1rem;
     }
@@ -38,8 +38,10 @@
         cursor: pointer;
     }
 
+    /* Border styles */
     .border {
-        border: 1px solid blue;
+        border: 2px solid transparent;
+        font-weight: 700;
     }
 
     .no-border {
@@ -47,14 +49,24 @@
         background-color: transparent;
     }
 
+    .border-peach {
+        border-color: #D98684;
+    }
+
+    .border-yellow {
+        border-color: #FAD9A1;
+    }
+
+    /* Button shape */
     .round {
         border-radius: 100px;
     }
 
     .square {
-        border-radius: 5px;
+        border-radius: 0.25rem; /* 4px */
     }
 
+    /* Text colors */
     .text-white {
         color: #fff;
     }
@@ -63,11 +75,28 @@
         color: #2b2b2b;
     }
 
+    .text-peach {
+        color: #D98684;
+    }
+
+    .text-yellow {
+        color: #FAD9A1;
+    }
+
+    /* Background colors */
     .bg-cyan {
         background-color: cyan;
     }
 
+    .bg-peach {
+        background-color: #D98684;
+    }
+
+    .bg-yellow {
+        background-color: #FAD9A1;
+    }
+
     .bg-none {
-        background-color: none;
+        background-color: transparent;
     }
 </style>
--- src/routes/ui-kit/+page.svelte
+++ src/routes/ui-kit/+page.svelte
@@ -4,13 +4,60 @@
 
 <a href="/" class="back">Back</a>
 
-<h1>Buttons</h1>
-<Button bgColor="bg-cyan">Click me</Button>
-<Button textColor="text-black" border={true} />
+<div class="container">
+    <main>
+        <h1 class="btn-title">Buttons</h1>
+        <!-- Square buttons -->
+        <section>
+            <div class="wrapper">
+                <h2>Peach</h2>
+                <Button textColor="text-white" bgColor="bg-peach">Click me</Button>
+                <Button textColor="text-peach" border={true} borderColor="border-peach"/>
+                <Button shape="round" textColor="text-white" bgColor="bg-peach">Click me</Button>
+                <Button shape="round" textColor="text-peach" border={true} borderColor="border-peach"/>
+            </div>
+            
+            <div class="wrapper">
+                <h2>Yellow</h2>
+                <Button textColor="text-white" bgColor="bg-yellow">Click me</Button>
+                <Button textColor="text-yellow" border={true} borderColor="border-yellow"/>
+                <Button shape="round" textColor="text-white" bgColor="bg-yellow">Click me</Button>
+                <Button shape="round" textColor="text-yellow" border={true} borderColor="border-yellow"/>
+            </div>
+        </section>
+
+        <!-- Round buttons -->
+        <section>
+            <Button shape="round" textColor="text-white" bgColor="bg-peach">Click me</Button>
+            <Button shape="round" textColor="text-black" border={true} />
+        </section>
+    </main>
+</div>
 
 <style>
-    h1 {
+    .container {
+        width: 90%;
+        margin: 0 auto;
+        box-shadow: 0 2px 8px #0000001e;
+        border-radius: 50px 50px 0 0;
+        padding: 1rem;
+    }
+
+    section {
+        margin-bottom: 2rem;
+    }
+
+    .wrapper {
+        margin-bottom: 5rem;
+    }
+
+    .btn-title {
         text-align: center;
+        background-color: #bebebe;
+        padding: 0.5rem 1rem;
+        width: 50%;
+        margin: 0 auto;
+
     }
 
     .back {
